68ac0a1188a69fb9dd00a849b30b2c78fe020096
d4t06 <huudat01234560@gmail.com>
2025-03-27 11:34:03 +0700

    update cors

--- src/config/corsOption.js
+++ src/config/corsOption.js
@@ -1,10 +1,12 @@
-const whiteList = [process.env.WHITE_LIST ?? '', "https://d4t06.github.io"];
+const whiteList =
+   typeof process.env.WHITE_LIST.split(", ") === "object"
+      ? process.env.WHITE_LIST.split(", ")
+      : [];
 
 const corsOptions = {
    // Credential: true,
    // cb(error, options) params
    origin: (origin, cb) => {
-      console.log("check origin", origin, whiteList);
       if (whiteList.indexOf(origin) !== -1 || !origin) cb(null, true);
       else cb("[error]: Not allowed by cors");
    },
